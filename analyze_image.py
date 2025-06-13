d00baad9d416514c14e2ed5d0bfd7e7d04ee6864
adolphasg <adolphasg@gmail.com>
2025-06-13 01:45:32 -0500

    analyze_image update

--- analyze_image.py
+++ analyze_image.py
@@ -36,7 +36,7 @@ def analyze_image(file_path, branch):
     item = {
         'filename': filename,
         'labels': labels,
-        'timestamp': datetime.utcnow().isoformat(),
+        'timestamp': datetime.datetime.utcnow().isoformat(),
         'branch': branch
     }
 
